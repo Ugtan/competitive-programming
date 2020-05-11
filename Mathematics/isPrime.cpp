32922358474282adf3b4311dc453f05da965fe42
Ugtan <umang.taneja0173@gmail.com>
2020-05-11 21:36:44 +0530

    Check if the number is prime or not

---
+++ Mathematics/isPrime.cpp
@@ -0,0 +1,21 @@
+// To find if the given number is prime or not
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+bool isPrime(int n) {
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0)
+      return false;
+  }
+  return true;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  isPrime(n) ? cout << "True\n" : cout << "False\n";
+  return 0;
+}
