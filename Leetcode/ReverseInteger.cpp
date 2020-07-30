117496052cf0dfe15f7b65a9d2fd91f6f6c48961
Ugtan <umangtaneja@protonmail.com>
2020-07-30 23:54:15 +0530

    Reverse the given integer.

---
+++ Leetcode/ReverseInteger.cpp
@@ -0,0 +1,17 @@
+// Reverse the given interger.
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        long result = 0;
+        while(x > 0 || x < 0) {
+            result = result * 10 + (x % 10);
+            x = x / 10;
+        }
+        if (result > INT_MAX || result < INT_MIN)
+            return 0;
+        
+        return (int)result;
+    }
+};
