5871ed7e4b49a0f3621628151b1a094076e984ca
Ugtan <umangtaneja@protonmail.com>
2020-08-03 23:49:25 +0530

    Find two numbers that add upto target.

---
+++ Leetcode/TwoSum2.cpp
@@ -0,0 +1,18 @@
+// find two numbers such that they add up to a specific target number.
+// Array is already sorted.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target)
+                return {i + 1, j + 1};
+            else if (numbers[i] + numbers[j] < target)
+                i++;
+            else
+                j--;
+        }
+        return {};
+    }
+};
